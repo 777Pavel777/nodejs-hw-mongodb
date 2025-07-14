73a4b44112ac4d4a6e728dab7f5a386c21baa0df
777Pavel777 <cross1295@icloud.com>
2025-07-14 19:46:10 +0300

    edits

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: './.env' });
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
